2ec10e49bd8394ccc54019b06632f83a10fa9edd
Carl Holmberg <holmberg.carl.j@gmail.com>
2020-04-22 22:22:11 +0200

    changed team to teams

--- packages/backend/src/modules/team/teams.query.ts
+++ packages/backend/src/modules/team/teams.query.ts
@@ -6,7 +6,7 @@ import DBTeam from '../../entities/team';
 import GQLTeam from './team';
 
 @Resolver()
-export default class TeamQueryResolver {
+export default class TeamsQueryResolver {
 	@Query(() => [GQLTeam])
 	async teams(): Promise<GQLTeam[]> {
 		const teamRepository = getRepository(DBTeam);
