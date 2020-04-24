724626e7dbc45df3d0bfddfc5b82eef4f58d8576
Aaron 'Nox' Sandgren <aaron.sandgren@live.se>
2020-04-24 17:19:53 +0200

    nogger är en god glass

--- packages/frontend/src/screens/app/app-view.tsx
+++ packages/frontend/src/screens/app/app-view.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import { Link, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
 
 import GuestRoute from './components/guest-route';
 import PrivateRoute from './components/private-route';
 import { useAuth } from './contexts/auth-context';
 import AuthScreen from './screens/auth';
+import HomeScreen from './screens/home';
 import UsersScreen from './screens/users';
 
 const AppView = () => {
 	const { isAuthenticated, logout, user } = useAuth();
 
 	return (
-		<>
-			<ul>
+		<div style={{ background: 'black', height: '100vh' }}>
+			<Normalize />
+			<ul style={{ margin: '0' }}>
 				<li>
 					<Link to="/users">Users</Link>
 				</li>
@@ -32,8 +35,9 @@ const AppView = () => {
 			<Switch>
 				<PrivateRoute path="/users" component={UsersScreen} />
 				<GuestRoute path="/auth" component={AuthScreen} />
+				<Route exact path="/" component={HomeScreen} />
 			</Switch>
-		</>
+		</div>
 	);
 };
 
