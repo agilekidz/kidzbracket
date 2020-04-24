78e59adc66b71adc92405638ef86288c523e3330
Carl Holmberg <holmberg.carl.j@gmail.com>
2020-04-24 23:37:41 +0200

    Added second-team field

--- packages/backend/src/modules/match/second-team.field.ts
+++ packages/backend/src/modules/match/second-team.field.ts
@@ -1,1 +1,16 @@
-//
+import { FieldResolver, Resolver, ResolverInterface, Root } from 'type-graphql';
+import { getRepository } from 'typeorm';
+
+import DBMatch from '../../entities/match';
+
+import GQLMatch from './match';
+
+@Resolver(() => GQLMatch)
+export default class SecondTeamResolver implements ResolverInterface<GQLMatch> {
+	@FieldResolver()
+	async secondTeam(@Root() { id }: GQLMatch) {
+		const matchRepository = getRepository(DBMatch);
+		const match = await matchRepository.findOne({ where: { id }, relations: ['secondTeam'] });
+		return match?.secondTeam;
+	}
+}
