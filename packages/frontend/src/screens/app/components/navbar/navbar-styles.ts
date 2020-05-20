ec47aa31c9dc258b595ec3239e2db3b884efc97a
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-05-20 12:43:23 +0200

    switched to ant layout and menu

--- packages/frontend/src/screens/app/components/navbar/navbar-styles.ts
+++ packages/frontend/src/screens/app/components/navbar/navbar-styles.ts
@@ -1,34 +1,6 @@
-import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-const LinkStyling = css`
-	text-decoration: none;
-	padding: 20px 15px;
-	display: block;
-	color: white;
-	transition: all 0.2s;
-	cursor: pointer;
-
-	&:hover {
-		box-shadow: inset 0 0 10px #000000;
-	}
-`;
-
-export const StyledLink = styled(Link)`
-	${LinkStyling}
-`;
-
-export const StyledButton = styled.button`
-	${LinkStyling}
-`;
-
-export const Navbar = styled.div`
-	background: #2b2d42;
+export const MenuWrapper = styled.div`
 	display: flex;
 	justify-content: space-between;
-	flex-grow: 1;
-`;
-
-export const Navbox = styled.div`
-	display: flex;
 `;
